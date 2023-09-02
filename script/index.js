b239f93074e055f2c91b4511f87a7144c4c0b505
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-09-01 22:25:16 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -34,24 +34,4 @@ document.addEventListener("DOMContentLoaded", function () {
       const itemWidth = document.querySelector(".carousel-item").clientWidth;
       slideContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
    }
-   function avancarSlide() {
-      if (currentIndex < 2) {
-         currentIndex++;
-      } else {
-         currentIndex = 0;
-      }
-      updateCarousel();
-   }
-   
-   let autoAdvanceInterval;
-   function iniciarTransicaoAutomatica() {
-      autoAdvanceInterval = setInterval(avancarSlide, 2000); // 5000 milissegundos = 5 segundos
-   }
-   
-   // Chame a função para iniciar a transição automática quando a página for carregada
-   document.addEventListener("DOMContentLoaded", function () {
-      // ...
-   
-      iniciarTransicaoAutomatica();
-   });
 });
